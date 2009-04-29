f83c6184057d3fbbbea339ac12f76f1fa79f7828
Ray Cromwell <cromwellian@gmail.com>
2009-04-29 00:03:09 +0000

    Sample test case

---
+++ gwtquery-core/src/test/java/gwtquery/client/GwtQueryCoreTest.java
@@ -0,0 +1,13 @@
+package gwtquery.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ *
+ */
+public class GwtQueryCoreTest extends GWTTestCase {
+
+  public String getModuleName() {
+    return "gwtquery.GwtQuery";
+  }
+}
