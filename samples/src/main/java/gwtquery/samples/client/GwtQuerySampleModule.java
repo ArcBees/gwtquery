31d1c7cae609b4adb17cfee60d112474576dd054
Ray Cromwell <cromwellian@gmail.com>
2009-07-27 09:01:20 +0000

    Update to codehaus gwt-maven-plugin. Update to GWT 1.7.0. Update module files to properly include Plugins.gwt.xml module. Disabled tests for now (JUnitShell not launching properly from new plugin). Fixed unbound deferred bindings.

--- samples/src/main/java/gwtquery/samples/client/GwtQuerySampleModule.java
+++ samples/src/main/java/gwtquery/samples/client/GwtQuerySampleModule.java
@@ -3,11 +3,8 @@ package gwtquery.samples.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.query.client.GQuery;
-import static com.google.gwt.query.client.GQuery.$;
 import com.google.gwt.query.client.Selector;
 import com.google.gwt.query.client.Selectors;
-import static com.google.gwt.query.client.css.CSS.TOP;
-import static com.google.gwt.query.client.css.CSS.VERTICAL_ALIGN;
 
 
 public class GwtQuerySampleModule implements EntryPoint {
