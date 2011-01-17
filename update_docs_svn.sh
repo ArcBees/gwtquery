b658f0f2908bed826ffce29cc9f11d3a15ce7d1c
Manolo Carrasco <manolo@apache.org>
2011-01-17 09:15:40 +0000

    update javadocs and demos deployed in svn

--- update_docs_svn.sh
+++ update_docs_svn.sh
@@ -2,23 +2,25 @@
 
 # Script to update javadoc deployed in svn
 
-cp -r gwtquery-core/target/apidocs/* gwtquery-core/javadoc/
+p=`pwd`
+
+cd gwtquery-core
+mvn javadoc:javadoc
+cd $p
+
+cp -r gwtquery-core/target/site/apidocs/* gwtquery-core/javadoc/
 
 for i in `find gwtquery-core/javadoc/ -type d | grep -v .svn | sed -e 's#gwtquery-core/javadoc/##g' `
 do
-   [ ! -d gwtquery-core/target/apidocs/$i ] && svn delete gwtquery-core/javadoc/$i 
+   [ ! -d gwtquery-core/target/site/apidocs/$i ] && svn delete gwtquery-core/javadoc/$i 
 done
 
 for i in `find gwtquery-core/javadoc/ -type f | grep -v .svn | sed -e 's#gwtquery-core/javadoc/##g' `
 do
-   [ ! -f gwtquery-core/target/apidocs/$i ] && svn delete gwtquery-core/javadoc/$i 
+   [ ! -f gwtquery-core/target/site/apidocs/$i ] && svn delete gwtquery-core/javadoc/$i 
 done
 
 find gwtquery-core/javadoc/  | grep -v .svn | xargs svn add
 
 find gwtquery-core/javadoc/ -type f -name "*html" -exec svn propset svn:mime-type text/html '{}' ';'
-
-
-
-
 
