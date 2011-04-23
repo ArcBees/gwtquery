453e494fa3093d9b97c779050ef5df1e2c5021db
Julien Dramaix <julien.dramaix@gmail.com>
2011-04-23 11:16:01 +0000

    remove unused import

--- gwtquery-core/src/main/java/com/google/gwt/query/client/js/JsCache.java
+++ gwtquery-core/src/main/java/com/google/gwt/query/client/js/JsCache.java
@@ -1,6 +1,5 @@
 package com.google.gwt.query.client.js;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArrayString;
 
