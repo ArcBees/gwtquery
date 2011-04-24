bb530cc05c9057db1dd5663ebf05a9bb182631f5
Julien Dramaix <julien.dramaix@gmail.com>
2011-04-24 20:05:19 +0000

    use string format for rgb()

--- samples/src/main/java/gwtquery/samples/client/effects/ColorEffectsSample.java
+++ samples/src/main/java/gwtquery/samples/client/effects/ColorEffectsSample.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2011, The gwtquery team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.samples.client.effects;
 
 import static com.google.gwt.query.client.GQuery.$;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.query.client.Function;
-import com.google.gwt.query.client.Properties;
 import com.google.gwt.query.client.css.CSS;
-import com.google.gwt.query.client.plugins.effects.PropertiesAnimation.Easing;
 
 public class ColorEffectsSample implements EntryPoint {
 
@@ -29,7 +42,7 @@ public class ColorEffectsSample implements EntryPoint {
           .delay(200)
           .animate("borderColor: '#ff0000'",1000)
           .delay(200)
-          .animate(Properties.create().$$("color","rgb(255, 255, 255)"), 1000, Easing.SWING);
+          .animate("color:'rgb(255, 255, 255)'", 1000);
       }
     
     });
