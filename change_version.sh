1402ed8bc263afa4c378ef532e931da8d593d967
Manolo Carrasco <manolo@apache.org>
2011-05-05 11:55:37 +0000

    preparing M3, and adding classifiers for 2.2.0 and 2.3.0

---
+++ change_version.sh
@@ -0,0 +1,9 @@
+
+[ -z "$1" ] && exit
+
+for i in pom.xml */pom.xml
+do
+    V=`head -20 $i | grep "<version>" | head -1 | cut -d ">" -f2 | cut -d "<" -f1`
+    perl -pi -e "s;<version>$V</version>;<version>$1</version>;" $i
+    echo $V $i
+done
