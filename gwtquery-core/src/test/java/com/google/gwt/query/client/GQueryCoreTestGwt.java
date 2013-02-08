34c793e9f06b2ba864b9e92856e69f43e80c4733
Manuel Carrasco Moñino <manuel.carrasco.m@gmail.com>
2013-02-08 13:14:11 +0100

    Remove unused import

--- gwtquery-core/src/test/java/com/google/gwt/query/client/GQueryCoreTestGwt.java
+++ gwtquery-core/src/test/java/com/google/gwt/query/client/GQueryCoreTestGwt.java
@@ -28,7 +28,6 @@ import java.util.List;
 import com.google.gwt.query.client.GQuery.Offset;
 import junit.framework.Assert;
 
-import com.google.gwt.dev.jjs.impl.AssertionNormalizer;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.InputElement;
