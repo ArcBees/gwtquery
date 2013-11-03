a4804be0086798610f23bf1b386641b6daa3ff2a
Manuel Carrasco Moñino <manuel.carrasco.m@gmail.com>
2013-11-03 12:40:47 +0100

    remove temporary data to avoid memory leaks

--- gwtquery-core/src/main/java/com/google/gwt/query/client/plugins/effects/Transitions.java
+++ gwtquery-core/src/main/java/com/google/gwt/query/client/plugins/effects/Transitions.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2013, The gwtquery team.
  *
+ * Copyright 2013, The gwtquery team.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
@@ -286,6 +286,8 @@ public class Transitions extends GQuery {
     // schedule setting css animated properties so as we are sure css transition property has been set.
     delay(0, new Function(){public void f() {
       css(p);
+      // prevent memory leak
+      removeData(TRANSFORM);
     }});
     
     // restore oldTransitions in the element, and use the queue to prevent more effects being run.
